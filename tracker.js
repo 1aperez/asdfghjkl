6547eae4cf997099fce1a34cd158b8157b38274d
1aperez <42749606+1aperez@users.noreply.github.com>
2018-11-28 14:00:19 -0600

    Add files via upload

--- tracker.js
+++ tracker.js
@@ -5,8 +5,25 @@ function Tracker(){
   let percent = [];
 
   //implement all six functions below
-
-
-
-
-}
+  function numOfSeeds()
+  function addSeed()
+  function removeSeed()
+  function reportHealth(){
+    let sum=0;
+    let count= 0;
+    for( let i=0; i<percent.length; i++){ 
+      if (percent[i] !=0){sum =sum +percent[i];}
+      count++;
+    }
+  }
+  function update(){
+   for(let i=0; i<seeds.length; i++){
+     let r= Math.random()
+     if (r>seeds[i] ){
+       seeds[i]=r
+       else(r<0.03){
+         seeds[i]=null;
+         percent[i]=0;
+       }
+     }
+   }
