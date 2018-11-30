bc3dcf2505acaf8a72b54ba0b9e2a2fbe3aad39c
1aperez <42749606+1aperez@users.noreply.github.com>
2018-11-30 13:55:32 -0600

    Add files via upload

--- user.js
+++ user.js
@@ -7,14 +7,48 @@ function User(tracker, torrent){
  let downloadspeed= 0;
  let uploadspeed=0;
   //Type all of your code in here.
-function startdownload(){}
+function startdownload(){
 
-function updateTracker(){}
+}
 
-function checkprogress(){}
+function updateTracker(){
+  update()
+}
 
-function leech(){}
+function checkprogress(){
+function Range(list){
+  let min = findMin(list)
+  let max = findMax(list)
+  return (min,max)
+}
+function findMax(list){
+  let max= list(0)
+  for (let i=0; i<torrent.length; i++){
+    if(list [i]> max){
+      max= list[i]
+    }
+  }
+  return max
+}
+}
+function findMin(list){
+  let min= list(0)
+  for (let i=0; i<torrent.length; i++){
+    if(list [i]> min){
+      min= list[i]
+      return min
+}
+  }
+}
+function leech(){
+  state= "leech";
+  downloadspeed = Math.random();
+  uploadspeed = 0;
+}
 
-function seed(){}
-
+function seed(){
+  state = "seed"
+  downloadspeed;
+  uploadspeed;
+}
 }
